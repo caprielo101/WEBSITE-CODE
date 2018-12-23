a40c6f0ec1382fe91eaafb523318c2dddd486be6
caprielo101 <caprielo101@gmail.com>
2018-12-23 23:41:53 +0700

    UI/UX changes
    More changes for better UI/UX (home footer, resizing media < 500px, order js)

--- JS/Promo.js
+++ JS/Promo.js
@@ -1,34 +1,36 @@
 var code = ["","AVLaunching", "FirstMonthAV"];
 // var a = -1;
 // var input = else if (a!=-1) {
-				// code.splice(a, 1);
+// code.splice(a, 1);
 
 function validate(){
-			var tempName = document.getElementById("form-name").value;
-			var tempPhone = document.getElementById("form-phone").value;
-			var tempError = document.getElementById("error");
-			var	tempAddress = document.getElementById("form-address").value;
-			var tempQuantity = document.getElementById("form-quantity").value;
-			var tempCode = document.getElementById("form-promo").value;
+		var tempName = document.getElementById("form-name").value;
+		var tempPhone = document.getElementById("form-phone").value;
+		var tempError = document.getElementById("error");
+		var	tempAddress = document.getElementById("form-address").value;
+		var tempQuantity = document.getElementById("form-quantity").value;
+		var tempCode = document.getElementById("form-promo").value;
 
-			// a = code.indexOf(tempCode);
+		// a = code.indexOf(tempCode);
 
-			if (tempName == "") {
-				tempError.innerHTML = "Name must be filled.";
-			} else if (tempPhone == ""){
-				tempError.innerHTML = "Phone number must be filled.";
-			} else if (tempAddress.length < 8) {
-				tempError.innerHTML = "Address is too short.";
-			} else if (tempQuantity < 1){
-				tempError.innerHTML = "Please enter a valid amount.";
-			} else if (!code.includes(tempCode)){
-				tempError.innerHTML = "Please enter a valid promo code.";
-			} else{
-				tempError.innerHTML = "";
-				alert("Registered");
-				window.location.reload();
-			}
-		}	
+		if (tempName == "") {
+			tempError.innerHTML = "Name must be filled.";
+		} else if (tempPhone == ""){
+			tempError.innerHTML = "Phone number must be filled.";
+		} else if(isNaN(tempPhone)){
+			tempError.innerHTML = "Your phone number needs to be numeric.";
+		} else if (tempAddress.length < 8) {
+			tempError.innerHTML = "Address is too short.";
+		} else if (tempQuantity < 1){
+			tempError.innerHTML = "Please enter a valid amount.";
+		} else if (!code.includes(tempCode)){
+			tempError.innerHTML = "Please enter a valid promo code.";
+		} else{
+			tempError.innerHTML = "";
+			alert("Registered");
+			window.location.reload();
+		}
+	}	
 
 
 
