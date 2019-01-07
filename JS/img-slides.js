69bdee37c239f630115d0ee89db92b06ae2aadbf
caprielo101 <caprielo101@gmail.com>
2019-01-07 22:26:10 +0700

    Responsive Gallery
    title

--- JS/img-slides.js
+++ JS/img-slides.js
@@ -5,6 +5,8 @@ carousel();
 function carousel(){
     var i;
     var x = document.getElementsByClassName("img-slides");
+    var y = document.getElementById("img-slides");
+
     for (i = 0; i< x.length; i++){
         x[i].style.display = "none";
     }
@@ -12,7 +14,13 @@ function carousel(){
     if (slideIndex > x.length) {
         slideIndex = 1;
     }
-    x[slideIndex-1].style.display = "block";
+    // x[slideIndex-1].style.display = "block";
+    if(window.innerWidth <= 500){
+        x[slideIndex-1].style.display = "inline-block";
+    } else x[slideIndex-1].style.display = "flex";
     setTimeout(carousel, 2000); 
     // image timer for x milliseconds
+
+    
+    
 }
